a11f9f72ea4755d47308b6c6f9ea5481c593fa36
arye-dov-eidelman <aryedov.e@gmail.com>
2018-10-11 17:48:57 -0400

    Added render and some erb tags

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,9 @@ Rails.application.routes.draw do
   root 'demo#index'
 
   get 'demo/index'
+  get 'demo/hello'
+  get 'demo/other_hello'
+  get 'demo/lynda'
 
  
   match 'blog', to: "blog#index", via: :get
