5ecfc51bd88231773868ec50f52b0da0d72443e0
arye-dov-eidelman <aryedov.e@gmail.com>
2018-10-11 17:31:51 -0400

    Adding one file I missed in the last commit

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,24 @@
 Rails.application.routes.draw do
+
+  root 'demo#index'
+
   get 'demo/index'
+
+ 
+  match 'blog', to: "blog#index", via: :get
+  match 'blog/new', to: "blog#new", via: :get
+  match 'blog/create', to: "blog#create", via: :post
+  match 'blog/edit/:id', to: "blog#edit", via: :get
+  match 'blog/update/:id', to: "blog#update", via: :patch
+  match 'blog/publish/:id', to: "blog#publish", via: :post
+  match 'blog/unpublish/:id', to: "blog#unpublish", via: :post
+  match 'blog/show/:id', to: "blog#show", via: :get
+  match 'blog/delete/:id', to: "blog#delete", via: :delete
+
+
+  # defoult route
+  # get ':controller(/:action(/:id))'
+
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
